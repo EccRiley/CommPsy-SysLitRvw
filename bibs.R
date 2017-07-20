eb2bc2de7f0ba895c21cadab82666a7801c3a362
EccRiley <rms7@pdx.edu>
2017-07-19 21:31:18 -0700

    Amend previous commit to reformat parallel set graphics in bibs.R

--- bibs.R
+++ bibs.R
@@ -22,10 +22,10 @@ knitr::opts_chunk$set(
     fig.height = 5,
     fig.path = "graphics/bibs/rplot-",
     fignos = TRUE,
-    dev = c('pdf', 'png'),
-    fig.retina = 6,
-    Rplot = NULL,
-    Rplot_whbg = TRUE,
+    dev = 'pdf',
+    # fig.retina = 6,
+    Rplot = TRUE,
+    Rplot_whbg = NULL,
     hideCap = FALSE,
     figPath = FALSE,
     fpath = NULL)
@@ -44,9 +44,6 @@ op <- par(no.readonly = TRUE) ## current values for settable plotting params (se
 pmar <- par(mar = c(5, 4, 4, 2))
 sci <- colorRampPalette(pal_sci[c(1, 2, 4, 5, 8)])
 # panderOptions("table.emphasize.rownames", "FALSE")
-
-pp <- c(pal_sci, pal_lz, pal_lancet, pal_my[3:16])
-pp <- pp[c(20, 6, 2, 9, 10, 24, 25, 11, 17, 26, 3, 27, 29, 5, 31, 12, 18, 32, 33, 34, 13, 15, 1, 7, 19, 14, 21, 22, 23, 4, 8)]
 
 # rpm()
 
@@ -1472,7 +1469,7 @@ hideLabs(x = parset.smthds, labs = labs(subtitle = "Sampling Methods"))
 #'
 #' \newpage
 #'
-#' # \LARGE{\textsc{Ecological Levels of Analysis}}
+#' # \textsc{Ecological Network Analysis: Data Analytic Approaches}
 #'
 #' \Frule
 #'
@@ -1523,8 +1520,11 @@ parset.eco <- ggparset2(list("Levels of Analysis",
     scale_colour_manual(values = c(pscat, peco), guide = FALSE) +
     thm_Rtft(ticks = FALSE, ytext = FALSE)
 hideLabs(x = parset.eco, labs = labs(subtitle = "Ecological Levels of Analysis"))
+
 #'
 #' \newpage
+#'
+#' # \textsc{Ecological Network Analysis}
 #'
 #+ ecoLvlsData
 ## SEE "MAPlvls.R" FOR MORE DETAILED COMMENTARY ON THE REMAINING CODE BELOW ##
@@ -1544,16 +1544,15 @@ mplvls1$nsys <- apply(mplvls1[, 6:8], 1, sum)
 
 rownames(mplvls1) <- mplvls1[, 1]
 mplvls <- mplvls1[, -1, drop = FALSE]
-
 #'
-#' # Levels of Analysis
+#' # Ecological Levels of Analysis
 #'
 #+ clrs_labs
 
 # COLORS & LABELS ----------------
 
-vclrs <- rev(grad(1:10, p = nord_polar))[4:7]
-vtclrs <- rev(grad(1:10, p = nord_polar))[6:9]
+vclrs <- rev(grad(1:10))[4:7]
+vtclrs <- rev(grad(1:10))[6:9]
 
 catpal85 <- adjustcolor(catpal, alpha.f = 0.85)
 lcvclrs <- c("1 = vclrs[1]; 2 = vclrs[2]; 3 = vclrs[3]; 4 = vclrs[4]; 'S3' = catpal[1]; 'S4' = catpal[2]")
@@ -1707,12 +1706,12 @@ lfrq <- Rtdf(lfrq3, names = c("lvl", "Freq"))
 library(igraph)
 lnetg <- graph_from_data_frame(lnet, directed = FALSE, vertices = lfrq)
 V(lnetg)$size <- V(lnetg)$Freq*1.5
-lnetcol <- mpal(lfrq, p = nord_aurora, a = 0.8)
+lnetcol <- mpal(lfrq, a = 0.8)
 V(lnetg)$color <- lnetcol
 E(lnetg)$width <- 0.25
 #'
 
-#+ net_ecolvls
+#+ net_ecolvls_analysis
 # PLOT - ecolvls ----------------
 
 par(mar = rep(0, 4))
@@ -1748,30 +1747,15 @@ library(arcdiagram)
 
 ledges <- cbind(lnet$from, lnet$to)
 lvals <- V(lnetg)$Freq
-ldeg <- degree(lnetg)
-larcs <- .35*(lnetft %>% matrix())
+ldeg <- log(degree(lnetg))
+larcs <- lnetft %>% matrix()
 larcs <- ifelse(larcs == 0, NA, larcs) %>% na.omit()
 
 #+ arc_lvls, fig.height=3.75, fig.fullwidth=TRUE
 
 # PLOT - `lnet - arcplot()` ----------------
 par(mar = c(5, 0, 0, 0))
-arcplot(ledges, col.arcs = hsv(0, 0, 0.1, 0.075), pch.nodes = 21, bg.nodes = adjustcolor(lnetcol, alpha.f = 0.75), cex.nodes = log(ldeg[c("Individual", "Relationship", "Community", "Societal")])*0.37, col.nodes = lnetcol, lwd.nodes = 0.75, lwd.arcs = larcs, line = 1.25, cex.labels = 0.5, font = 1, col.labels = pal_my[20])
-
-#'
-#+ cbanl
-cbanl <- cb[cb$cat == "A-QT" | cb$cat == "A-QL",
-            c("bibkey", "scat", "cat", "code", "clab")] %>% droplevels()
-cbaqt <- cbanl[cbanl$cat == "A-QT", ] %>% droplevels()
-cbaql <- cbanl[cbanl$cat == "A-QL", ] %>% droplevels()
-
-alabs <- paste0(seq(1:length(levels(cbanl$clab))), " = ", levels(cbanl$clab))
-alabsqt <- paste0(seq(1:length(levels(cbaqt$clab))), " = ", levels(cbaqt$clab))
-alabsql <- paste0(seq(1:length(levels(cbaql$clab))), " = ", levels(cbaql$clab))
-
-kysanl <- cbanl[, c("bibkey", "clab")] %>% dplyr::rename("from" = bibkey, "to" = clab)
-kysaqt <- cbaqt[, c("bibkey", "clab")] %>% dplyr::rename("from" = bibkey, "to" = clab)
-kysaql <- cbaql[, c("bibkey", "clab")] %>% dplyr::rename("from" = bibkey, "to" = clab)
+arcplot(ledges, col.arcs = hsv(0, 0, 0.1, 0.075), pch.nodes = 21, bg.nodes = adjustcolor(lnetcol, alpha.f = 0.25), cex.nodes = ldeg, col.nodes = pal_my[19], lwd.nodes = 1, lwd.arcs = larcs, line = 1.25, cex.labels = 0.75, font = 1, col.labels = pal_my[20])
 
 #'
 #' \newpage
@@ -1848,7 +1832,7 @@ library(igraph) ## not necessary - but included as reminder and in case I move/c
 snetg <- graph_from_data_frame(snet, directed = FALSE, vertices = sfrq)
 V(snetg)$size <- V(snetg)$Freq*1.5
 # V(snetg)$label.size <- (V(snetg)$Freq*0.85
-snetcol <- mpal(sfrq, p = nord_aurora, a = 0.8)
+snetcol <- mpal(sfrq, a = 0.8)
 V(snetg)$color <- snetcol
 E(snetg)$width <- 0.25
 #'
@@ -1864,10 +1848,10 @@ plot(snetg, rescale = T, layout = ls1n, edge.arrow.size = .2, vertex.label.color
 #' \newpage
 #'
 #+ net_sysLvls, fig.fullwidth=TRUE, figPath=TRUE
-par(mar = c(0, 0, 0, 0), mfrow = c(1, 2))
-plot(lnetg, rescale = T, layout = ll3n, edge.arrow.size = .2, vertex.label.color = "#1a1e22", vertex.frame.color = NA, margin = c(0, -0.15, -0.15, 0.05)); box(which = "figure")
-plot(snetg, rescale = T, layout = ls1n, edge.arrow.size = .2, vertex.label.color = "#1a1e22", vertex.frame.color = NA, margin = c(-2.5, -0.65, -2, 0.05)); box(which = "figure")
-par(mfrow=c(1, 1))
+# par(mar = c(0, 0, 0, 0), mfrow = c(1, 2))
+# plot(lnetg, rescale = T, layout = ll3n, edge.arrow.size = .2, vertex.label.color = "#1a1e22", vertex.frame.color = NA, margin = c(0, -0.15, -0.15, 0.05)); box(which = "figure")
+# plot(snetg, rescale = T, layout = ls1n, edge.arrow.size = .2, vertex.label.color = "#1a1e22", vertex.frame.color = NA, margin = c(0, 0, 0, -0.5)); box(which = "figure")
+# par(mpar, mfrow = c(1, 1))
 
 
 #'
@@ -1897,7 +1881,7 @@ snetftp <- ifelse(snetft == 0, NA, snetft)
 sedges <- cbind(snet$from, snet$to)
 svals <- V(snetg)$Freq
 sdeg <- degree(snetg)
-sarcs <- .15*(snetft %>% matrix())
+sarcs <- snetft %>% matrix()
 sarcs <- ifelse(sarcs == 0, NA, sarcs) %>% na.omit()
 
 #+ arc_sys, fig.height=3.5, fig.fullwidth=TRUE
@@ -1905,11 +1889,150 @@ sarcs <- ifelse(sarcs == 0, NA, sarcs) %>% na.omit()
 # PLOT - `snetft-arcplot` ----------------
 
 par(mar = c(5, 0, 0, 0))
-arcplot(sedges, col.arcs = hsv(0, 0, 0.1, 0.06), pch.nodes = 21, bg.nodes = adjustcolor(snetcol, alpha.f = 0.5), cex.nodes = log(sdeg[c("Micro", "Meso-Exo", "Exo-Macro")])*0.3, col.nodes = snetcol, lwd.nodes = 0.75, lwd.arcs = sarcs, line = 1.25, cex.labels = 0.5, font = 1, col.labels = pal_my[20])
+arcplot(sedges, col.arcs = hsv(0, 0, 0.1, 0.06), pch.nodes = 21, bg.nodes = adjustcolor(snetcol, alpha.f = 0.5), cex.nodes = rev(sdeg)*0.25, col.nodes = snetcol, lwd.nodes = 0.75, lwd.arcs = sarcs, line = 1.25, cex.labels = 0.5, font = 1, col.labels = pal_my[20])
 #'
 #' \newpage
 #'
-#' # Data Analytic Approaches by Ecological Levels of Analysis
+#' ## \textsc{Network Analysis: Substantive Research Topics}
+#+ tlnet
+cbt <- cb[cb$cat == "TOPIC",
+          c("bibkey", "scat", "cat", "code", "clab")] %>% droplevels()
+cbt <- cbt[!duplicated(cbt), ]
+tlabs <- paste0(seq(1:length(levels(cbt$clab))), " = ", levels(cbt$clab))
+
+lt <- mplvls[, 1:4]
+lt$id <- rownames(lt)
+
+lcbt <- merge(lt, cbt, by.x = "id", by.y = "bibkey", all = FALSE)
+lcbt.t <- lcbt[, c("code", "l1", "l2", "l3", "l4")] %>% dplyr::rename("id" = code)
+lcbt.t.nd <- lcbt.t[!duplicated(lcbt.t), ]
+
+
+
+tlnet1 <- lcbt.t[, 1:2] %>% Rtdf(names = c(names(lcbt.t[,1:2]), "Freq"))
+tlnet1$l1 <- as.numeric(tlnet1$l1)
+tlnet1$l1 <- ifelse(tlnet1$l1 == 1, NA, "l1")
+tlnet1$Freq <- ifelse(tlnet1$Freq == 0, NA, tlnet1$Freq)
+tlnet1 <- na.omit(tlnet1)
+# tlnet1 <- tlnet1[!duplicated(tlnet1$id), ]
+names(tlnet1) <- c("from", "to", "Freq")
+tlnet2 <- lcbt.t[, c(1, 3)] %>% Rtdf(names = c(names(lcbt.t[, c(1, 3)]), "Freq"))
+tlnet2$l2 <- as.numeric(tlnet2$l2)
+tlnet2$l2 <- ifelse(tlnet2$l2 == 1, NA, "l2")
+tlnet2$Freq <- ifelse(tlnet2$Freq == 0, NA, tlnet2$Freq)
+tlnet2 <- na.omit(tlnet2)
+names(tlnet2) <- c("from", "to", "Freq")
+
+tlnet3 <- lcbt.t[, c(1, 4)] %>% Rtdf(names = c(names(lcbt.t[,c(1, 4)]), "Freq"))
+tlnet3$l3 <- as.numeric(tlnet3$l3)
+tlnet3$l3 <- ifelse(tlnet3$l3 == 1, NA, "l3")
+tlnet3$Freq <- ifelse(tlnet3$Freq == 0, NA, tlnet3$Freq)
+tlnet3 <- na.omit(tlnet3)
+names(tlnet3) <- c("from", "to", "Freq")
+
+tlnet4 <- lcbt.t[, c(1, 5)] %>% Rtdf(names = c(names(lcbt.t[,c(1, 5)]), "Freq"))
+tlnet4$l4 <- as.numeric(tlnet4$l4)
+tlnet4$l4 <- ifelse(tlnet4$l4 == 1, NA, "l4")
+tlnet4$Freq <- ifelse(tlnet4$Freq == 0, NA, tlnet4$Freq)
+tlnet4 <- na.omit(tlnet4)
+names(tlnet4) <- c("from", "to", "Freq")
+
+tlnet0 <- rbind(tlnet1, tlnet2, tlnet3, tlnet4)
+tlnet0$clab <- recode(tlnet0$from, rec.code2clab) ## "rec.code2clab" is from "MAPrqda.R"
+tlnet <- tlnet0[!duplicated(tlnet0), c("from", "to", "clab", "Freq")]
+
+#+ tlnet_llabs
+library(car)
+llabs <- c("'l1' = '.Individual'; 'l2' = '.Relationship'; 'l3' = '.Community'; 'l4' = '.Societal'")
+tlnet$to <- car::recode(tlnet$to, llabs)
+
+#+ tlfrq
+
+# tlfrq ----------------
+
+tlfrq1 <- tlnet[, 1] %>% as.character()
+tlfrq2 <- tlnet[, 2]
+tlfrq3 <- c(tlfrq1, tlfrq2)
+tlfrq <- Rtdf(tlfrq3, names = c("lvl", "Freq"))
+
+tv1 <- tlnet[, 3] %>% as.character()
+tv2 <- tlnet[, 2]
+tv3 <- c(tv1, tv2)
+tv <- Rtdf(tv3, names = c("id", "Freq"))
+tv[, 1] <- as.character(tv[, 1])
+# `tlnetg` ----------------
+
+library(igraph)
+tlnetg <- graph_from_data_frame(tlnet[, 1:2], directed = FALSE, vertices = tlfrq)
+V(tlnetg)$size <- V(tlnetg)$Freq*3
+tlnetcol <- mpal(tlfrq, a = 0.8)[-1:-4] %>% adjustcolor(alpha.f = 0.5)
+tvclrs <- c(adjustcolor(vclrs[1:4], alpha.f = 0.65), tlnetcol)
+V(tlnetg)$color <- tvclrs
+# E(tlnetg)$width <- 0.25
+E(tlnetg)$frq <- tlnet$Freq
+E(tlnetg)$width <- log(tlnet$Freq) + 1
+V(tlnetg)$name[-1:-4] <- seq(1:length(tv[-1:-4, 1]))
+V(tlnetg)$name[1:4] <- gsub("\\.", "", V(tlnetg)$name[1:4])
+tindex.g <- V(tlnetg)$name %>% length()
+tlblsize <- c(log(V(tlnetg)$size[1:4])*0.125, log(V(tlnetg)$size[5:tindex.g])*0.325)
+#'
+#'
+#+ tlnetg_cap, echo=FALSE
+tlabs <- gsub("&", "\\\\&", tlabs)
+tlabs <- paste(tlabs, collapse = ", ")
+
+tlnetg_cap <- paste0("Network Diagram Showing Relations among Substantive Research Topics (numbered graph nodes) Covered \\& Ecological Levels of Analysis (named graph nodes) Involved among the Reviewed Literature: \\textit{", tlabs, "}")
+# PLOTS - `tlnetg` (layout-0 & layout-2) ----------------
+
+#'
+#+ net_topics, out.height='4in', fig.fullwidth=TRUE, fig.cap=tlnetg_cap, figPath=TRUE
+
+par(mar = rep(0, 4))
+ltl <- layout_with_fr(tlnetg) %>% norm_coords()
+plot(tlnetg, rescale = T, layout = ltl, edge.arrow.size = .2, vertex.label.color = "#1a1e22", vertex.frame.color = c(vclrs[1:4], rep(NA, length(tlnetcol))), vertex.label.cex = tlblsize)
+
+l1tpsum <- tlnet[tlnet$to == "l1", "Freq"] %>% sum()
+l2tpsum <- tlnet[tlnet$to == "l2", "Freq"] %>% sum()
+l3tpsum <- tlnet[tlnet$to == "l3", "Freq"] %>% sum()
+l4tpsum <- tlnet[tlnet$to == "l4", "Freq"] %>% sum()
+
+l1tpWgtMu <- l1tpsum / length(unique(tlnet$from))
+l2tpWgtMu <- l2tpsum / length(unique(tlnet$from))
+l3tpWgtMu <- l3tpsum / length(unique(tlnet$from))
+l4tpWgtMu <- l4tpsum / length(unique(tlnet$from))
+
+l1tpRawMu <- tlnet[tlnet$to == "l1", "Freq"] %>% mean()
+l2tpRawMu <- tlnet[tlnet$to == "l2", "Freq"] %>% mean()
+l3tpRawMu <- tlnet[tlnet$to == "l3", "Freq"] %>% mean()
+l4tpRawMu <- tlnet[tlnet$to == "l4", "Freq"] %>% mean()
+
+tpMuAll <- mean(tlnet$Freq)
+# l1tpRawMu/tpMuAll
+# l1tpWgtMu/tpMuAll
+# l2tpRawMu/tpMuAll
+# l2tpWgtMu/tpMuAll
+#'
+#'
+#' \newpage
+#'
+#'
+#+ cbanl
+cbanl <- cb[cb$cat == "A-QT" | cb$cat == "A-QL",
+            c("bibkey", "scat", "cat", "code", "clab")] %>% droplevels()
+cbaqt <- cbanl[cbanl$cat == "A-QT", ] %>% droplevels()
+cbaql <- cbanl[cbanl$cat == "A-QL", ] %>% droplevels()
+
+alabs <- paste0(seq(1:length(levels(cbanl$clab))), " = ", levels(cbanl$clab))
+alabsqt <- paste0(seq(1:length(levels(cbaqt$clab))), " = ", levels(cbaqt$clab))
+alabsql <- paste0(seq(1:length(levels(cbaql$clab))), " = ", levels(cbaql$clab))
+
+kysanl <- cbanl[, c("bibkey", "clab")] %>% dplyr::rename("from" = bibkey, "to" = clab)
+kysaqt <- cbaqt[, c("bibkey", "clab")] %>% dplyr::rename("from" = bibkey, "to" = clab)
+kysaql <- cbaql[, c("bibkey", "clab")] %>% dplyr::rename("from" = bibkey, "to" = clab)
+#'
+#' \newpage
+#'
+#' ## Network Analysis: Data Analytic Approaches by Ecological Levels
 #'
 cba <- cb[cb$cat == "A-QT" | cb$cat == "A-QL",
           c("bibkey", "scat", "cat", "code", "clab")] %>% droplevels()
@@ -1993,7 +2116,7 @@ av[, 1] <- as.character(av[, 1])
 library(igraph)
 alnetg <- graph_from_data_frame(alnet[, 1:2], directed = FALSE, vertices = alfrq)
 V(alnetg)$size <- V(alnetg)$Freq*1.5
-alnetcol <- mpal(alfrq, p = nord_aurora, a = 0.8)[-1:-4] %>% adjustcolor(alpha.f = 0.5)
+alnetcol <- mpal(alfrq, a = 0.8)[-1:-4] %>% adjustcolor(alpha.f = 0.5)
 avclrs <- c(adjustcolor(vclrs[1:4], alpha.f = 0.65), alnetcol)
 V(alnetg)$color <- avclrs
 E(alnetg)$width <- 0.25
@@ -2273,8 +2396,8 @@ gg.invtl <- ggplot(inv, aes(x = year, y = 0, colour = cpv)) +
     scale_colour_manual(values = pcpv) + #, guide = FALSE) +
     geom_hline(yintercept = 0, size = 0.25, color = pal_nord$polar[7], alpha = 0.5) +
     geom_segment(aes(y = 0, yend = pos, x = year, xend = year),
-                 colour = pal_my[19], alpha = 0.45,
-                 na.rm = TRUE, size = 0.15) +
+                 colour = pal_my[19], alpha = 0.55,
+                 na.rm = TRUE, size = 0.2, linetype = 3) +
     geom_text(aes(y = pos, x = year, label = bibkey2), hjust = 0.5, vjust = 1,
               angle = 25, size = 2.5, fontface = "bold") + #, nudge_y = -0.05) +
     # geom_vline(xintercept = vawa, size = 0.45, color = pal_nord$polar[1], linetype = 3) +
@@ -2639,8 +2762,8 @@ gg.smwtl <- ggplot(smw, aes(x = year, y = 0, colour = cpv)) +
     scale_colour_manual(values = pcpv) + #, guide = FALSE) +
     geom_hline(yintercept = 0, size = 0.25, color = pal_nord$polar[7], alpha = 0.5) +
     geom_segment(aes(y = 0, yend = pos, x = year, xend = year),
-                 colour = pal_my[19], alpha = 0.45,
-                 na.rm = TRUE, size = 0.15) +
+                 colour = pal_my[19], alpha = 0.55,
+                 na.rm = TRUE, size = 0.2, linetype = 3) +
     geom_text(aes(y = pos, x = year, label = bibkey2), hjust = 0.5, vjust = 0,
               angle = 45, size = 2.5, fontface = "bold") + #, nudge_y = -0.05) +
     geom_text(aes(y = 0, x = year, label = year), check_overlap = TRUE,
@@ -2950,128 +3073,6 @@ kable(ks4eco, caption = "Levels of Analysis by Study (SMW-Inclusive Research)")
 pander(lvla4.eco)
 
 par(pmar)
-#'
-#' \newpage
-#'
-#' # Network Analysis of Ecological Levels of Analysis \& Topics Covered
-#'
-
-cbt <- cb[cb$cat == "TOPIC",
-          c("bibkey", "scat", "cat", "code", "clab")] %>% droplevels()
-cbt <- cbt[!duplicated(cbt), ]
-tlabs <- paste0(seq(1:length(levels(cbt$clab))), " = ", levels(cbt$clab))
-
-lt <- mplvls[, 1:4]
-lt$id <- rownames(lt)
-
-lcbt <- merge(lt, cbt, by.x = "id", by.y = "bibkey", all = FALSE)
-lcbt.t <- lcbt[, c("code", "l1", "l2", "l3", "l4")] %>% dplyr::rename("id" = code)
-lcbt.t.nd <- lcbt.t[!duplicated(lcbt.t), ]
-
-
-
-tlnet1 <- lcbt.t[, 1:2] %>% Rtdf(names = c(names(lcbt.t[,1:2]), "Freq"))
-tlnet1$l1 <- as.numeric(tlnet1$l1)
-tlnet1$l1 <- ifelse(tlnet1$l1 == 1, NA, "l1")
-tlnet1$Freq <- ifelse(tlnet1$Freq == 0, NA, tlnet1$Freq)
-tlnet1 <- na.omit(tlnet1)
-# tlnet1 <- tlnet1[!duplicated(tlnet1$id), ]
-names(tlnet1) <- c("from", "to", "Freq")
-tlnet2 <- lcbt.t[, c(1, 3)] %>% Rtdf(names = c(names(lcbt.t[, c(1, 3)]), "Freq"))
-tlnet2$l2 <- as.numeric(tlnet2$l2)
-tlnet2$l2 <- ifelse(tlnet2$l2 == 1, NA, "l2")
-tlnet2$Freq <- ifelse(tlnet2$Freq == 0, NA, tlnet2$Freq)
-tlnet2 <- na.omit(tlnet2)
-names(tlnet2) <- c("from", "to", "Freq")
-
-tlnet3 <- lcbt.t[, c(1, 4)] %>% Rtdf(names = c(names(lcbt.t[,c(1, 4)]), "Freq"))
-tlnet3$l3 <- as.numeric(tlnet3$l3)
-tlnet3$l3 <- ifelse(tlnet3$l3 == 1, NA, "l3")
-tlnet3$Freq <- ifelse(tlnet3$Freq == 0, NA, tlnet3$Freq)
-tlnet3 <- na.omit(tlnet3)
-names(tlnet3) <- c("from", "to", "Freq")
-
-tlnet4 <- lcbt.t[, c(1, 5)] %>% Rtdf(names = c(names(lcbt.t[,c(1, 5)]), "Freq"))
-tlnet4$l4 <- as.numeric(tlnet4$l4)
-tlnet4$l4 <- ifelse(tlnet4$l4 == 1, NA, "l4")
-tlnet4$Freq <- ifelse(tlnet4$Freq == 0, NA, tlnet4$Freq)
-tlnet4 <- na.omit(tlnet4)
-names(tlnet4) <- c("from", "to", "Freq")
-
-tlnet0 <- rbind(tlnet1, tlnet2, tlnet3, tlnet4)
-tlnet0$clab <- recode(tlnet0$from, rec.code2clab) ## "rec.code2clab" is from "MAPrqda.R"
-tlnet <- tlnet0[!duplicated(tlnet0), c("from", "to", "clab", "Freq")]
-
-#+ tlnet_llabs
-library(car)
-llabs <- c("'l1' = '.Individual'; 'l2' = '.Relationship'; 'l3' = '.Community'; 'l4' = '.Societal'")
-tlnet$to <- car::recode(tlnet$to, llabs)
-
-#+ tlfrq
-
-# tlfrq ----------------
-
-tlfrq1 <- tlnet[, 1] %>% as.character()
-tlfrq2 <- tlnet[, 2]
-tlfrq3 <- c(tlfrq1, tlfrq2)
-tlfrq <- Rtdf(tlfrq3, names = c("lvl", "Freq"))
-
-tv1 <- tlnet[, 3] %>% as.character()
-tv2 <- tlnet[, 2]
-tv3 <- c(tv1, tv2)
-tv <- Rtdf(tv3, names = c("id", "Freq"))
-tv[, 1] <- as.character(tv[, 1])
-# `tlnetg` ----------------
-
-library(igraph)
-tlnetg <- graph_from_data_frame(tlnet[, 1:2], directed = FALSE, vertices = tlfrq)
-V(tlnetg)$size <- V(tlnetg)$Freq*3
-tlnetcol <- mpal(tlfrq, a = 0.8)[-1:-4] %>% adjustcolor(alpha.f = 0.5)
-tvclrs <- c(adjustcolor(vclrs[1:4], alpha.f = 0.65), tlnetcol)
-V(tlnetg)$color <- tvclrs
-# E(tlnetg)$width <- 0.25
-E(tlnetg)$frq <- tlnet$Freq
-E(tlnetg)$width <- log(tlnet$Freq) + 1
-V(tlnetg)$name[-1:-4] <- seq(1:length(tv[-1:-4, 1]))
-V(tlnetg)$name[1:4] <- gsub("\\.", "", V(tlnetg)$name[1:4])
-tindex.g <- V(tlnetg)$name %>% length()
-tlblsize <- c(log(V(tlnetg)$size[1:4])*0.125, log(V(tlnetg)$size[5:tindex.g])*0.325)
-#'
-#'
-#+ echo=FALSE
-tlabs <- gsub("&", "\\\\&", tlabs)
-tlabs <- paste(tlabs, collapse = ", ")
-
-tlnetg_cap <- paste0("Network Diagram Showing Relations among Substantive Research Topics (numbered graph nodes) Covered \\& Ecological Levels of Analysis (named graph nodes) Involved among the Reviewed Literature: \\textit{", tlabs, "}")
-# PLOTS - `tlnetg` (layout-0 & layout-2) ----------------
-
-#'
-#+ net_topics, out.height='4in', fig.cap=tlnetg_cap, figPath=TRUE
-
-par(mar = rep(0, 4))
-ltl <- layout_with_fr(tlnetg) %>% norm_coords()
-plot(tlnetg, rescale = T, layout = ltl, edge.arrow.size = .2, vertex.label.color = "#1a1e22", vertex.frame.color = c(vclrs[1:4], rep(NA, length(tlnetcol))), vertex.label.cex = tlblsize)
-
-l1tpsum <- tlnet[tlnet$to == "l1", "Freq"] %>% sum()
-l2tpsum <- tlnet[tlnet$to == "l2", "Freq"] %>% sum()
-l3tpsum <- tlnet[tlnet$to == "l3", "Freq"] %>% sum()
-l4tpsum <- tlnet[tlnet$to == "l4", "Freq"] %>% sum()
-
-l1tpWgtMu <- l1tpsum / length(unique(tlnet$from))
-l2tpWgtMu <- l2tpsum / length(unique(tlnet$from))
-l3tpWgtMu <- l3tpsum / length(unique(tlnet$from))
-l4tpWgtMu <- l4tpsum / length(unique(tlnet$from))
-
-l1tpRawMu <- tlnet[tlnet$to == "l1", "Freq"] %>% mean()
-l2tpRawMu <- tlnet[tlnet$to == "l2", "Freq"] %>% mean()
-l3tpRawMu <- tlnet[tlnet$to == "l3", "Freq"] %>% mean()
-l4tpRawMu <- tlnet[tlnet$to == "l4", "Freq"] %>% mean()
-
-tpMuAll <- mean(tlnet$Freq)
-# l1tpRawMu/tpMuAll
-# l1tpWgtMu/tpMuAll
-# l2tpRawMu/tpMuAll
-# l2tpWgtMu/tpMuAll
 #'
 #' \newpage\onehalfspacing
 #'
